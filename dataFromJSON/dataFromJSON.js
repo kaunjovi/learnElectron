0607114a4a071237613d2703b14deabcf6d4db6c
kaunjovi <kaunjovi@gmail.com>
2016-09-11 00:34:10 +0530

    reading data from JSON file alrighty

--- dataFromJSON/dataFromJSON.js
+++ dataFromJSON/dataFromJSON.js
@@ -1,15 +1,22 @@
 var app = angular.module('myApp', []);
 
-app.controller('myCntrl', function ($scope , $http) {
+app.controller('myCntrl', function ($scope, $http) {
 
     console.log('Hello world.');
     $scope.greetings = 'Hello world.';
+
+    $scope.openAFile = function () {
+        console.log('open a file.');
+    };
+
 
     $http.get('./dataFromJSON.json')
         .success(function (data) {
             console.log(data.credentials);
             $scope.credentialCollection = data.credentials;
         });
+
+
 
 
 }); 
